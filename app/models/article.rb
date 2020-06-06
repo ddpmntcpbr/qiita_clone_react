94bdb7f64a2a0643e70a09a048a8776a56b457db
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-06-06 14:06:31 +0900

    Add validattion

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comment, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, uniqueness: { case_sensitive: true }
 end
