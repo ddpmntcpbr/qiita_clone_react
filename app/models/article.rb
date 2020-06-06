0519dc59ec9640d27de12f72eb6973a0d8e57de6
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-06-06 13:12:47 +0900

    Create ArticleLike model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comment
+  has_many :comment, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
