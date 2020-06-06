6a44a950dd1bc1c7d19b948f6e730fc4daea558a
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-06-06 12:25:27 +0900

    Change automatic file generation settings

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,15 @@ module QiitaCloneReact
       g.template_engine false
       g.javascripts false
       g.stylesheets false
-      g.helper false
-      g.test_framework :rspec
+      g.helper true
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
     end
+
+    config.api_only = true
   end
 end
